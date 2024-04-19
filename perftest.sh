f9053dcc4c02eba0dd1c38dc8675f17872bf99ba
Maciej Waszczuk <u2054054@warwick.ac.uk>
2024-04-19 12:59:13 +0100

    perftest script update

--- perftest.sh
+++ perftest.sh
@@ -6,9 +6,11 @@ version=$1
 n_particles=$2
 timestep=$3
 numthreads=$4
+nodes=$5
 
-# if version is sphexa
-if [ $version == "sphexa" ]
+# if version is sphexa or sphexa-cuda
+
+if [ $version == "sphexa" ] || [ $version == "sphexa-cuda" ]
 then
     sourcedir="../sph-exa-build/main/src/sphexa"
 else
@@ -18,8 +20,28 @@ fi
 # sbatch save output to var
 # echo "sbatch oneperf.sbatch --job-name=$1 --output=out/$1/$1_$2_$3_$i_%j.out $sourcedir/$1 $n_particles $timestep'"
 # jobname=$(sbatch oneperf.sbatch --job-name=$1 --output=out/$1/$1_$2_$3_$i_%j.out $sourcedir/$1 $n_particles $timestep)    
-jobname=$(sbatch oneperf.sbatch $sourcedir/$1 $n_particles $timestep $numthreads)
+
+# if mpi argument is passed
+# if [ $5 == "mpi" ]
+# then
+#     jobname=$(sbatch oneperfmpi.sbatch $sourcedir/$1 $n_particles $timestep $numthreads $5)
+# else
+#     jobname=$(sbatch oneperfmpi.sbatch $sourcedir/$1 $n_particles $timestep $numthreads)
+# fi
+if [ $version == "sphexa" ]
+then
+    jobname=$(sbatch oneperfmpi$nodes.sbatch $sourcedir/$version $n_particles $timestep $numthreads $nodes)
+else
+    jobname=$(sbatch oneperfmpi$nodes.sbatch $sourcedir/$version $n_particles $timestep $numthreads $nodes)
+fi
+# jobname=$(sbatch oneperf.sbatch $sourcedir/$1 $n_particles $timestep $numthreads)
+# change version if it's sphexa-cuda
+
+# if [ $version == "sphexa" ]
+# then
+#     version="sphexa_cuda_nogpu"
+# fi
 jobname=$(echo $jobname | grep -oP '(?<=\s)\d+')
-mkdir -p out/$1
-echo "slurm-$jobname.out" >> out/$1/$2-$3-$4-jobs.txt
+mkdir -p out/$version
+echo "slurm-$jobname.out" >> out/$version/$2-$3-$4-$5-no-out-jobs.txt
 echo $jobname
